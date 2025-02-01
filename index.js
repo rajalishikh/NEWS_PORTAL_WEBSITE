3e7bf7719df2f3fbaa957aeaf10b26b54ccf6814
rajalishikhe <nahidujjamanraj@gmail.com>
2025-02-01 17:28:33 +0600

    I bring the all details data

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const load_data=async()=>{
 load_data()
 // show data 
 function show_data_news(data){
-    console.log(data)
+    // console.log(data)
     const container=document.getElementById('news_container');
     const creteElement=document.createElement('div')
     creteElement.innerHTML=`<p onclick="details(${data.category_id})" class="hover:text-[#5D5FEF] hover:bg-[#EEEFFF] p-2">${data.category_name}</p>`
@@ -21,3 +21,12 @@ function show_data_news(data){
 
 }
 // function show details
+async function details(id){
+     let number=id.toString()
+     const bring_dataBy_id=await fetch(`https://openapi.programming-hero.com/api/news/category/0${number}`)
+     const convert_json2= await bring_dataBy_id.json()
+     convert_json2.data.forEach((detail)=>{
+        console.log(detail)
+     })
+
+}
